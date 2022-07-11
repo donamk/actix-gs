273a7fbf450a922ca25e51c688d690799e9b8656
Donam Kim <sstmih@gmail.com>
2022-07-11 22:37:27 +0900

    feat: add server

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use actix_web::{get, post, App, HttpResponse, HttpServer, Responder, web};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hello", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
