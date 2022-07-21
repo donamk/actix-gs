7944d75edf3f3241a2f97221c6dd320be8822a41
Donam Kim <sstmih@gmail.com>
2022-07-22 01:09:13 +0900

    http get with struct & json

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,38 @@
 use awc::Client;
 use log::LevelFilter;
+use serde::Deserialize;
+use std::error::Error;
+
+#[derive(Deserialize)]
+struct MyIp {
+    origin: String,
+}
 
 #[actix_rt::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn Error>> {
     env_logger::Builder::new()
         .filter(None, LevelFilter::Debug)
         .init();
 
     let client = Client::default();
 
-    let res = client
+    let mut res = client
         .get("http://httpbin.org/ip")
         .insert_header(("Content-Type", "application/json"))
         .send()
-        .await;
+        .await?;
+    log::debug!("Response: {:?}", res);
 
-    if res.is_err() {
-        log::error!("Wikipedia did not return expected image");
-    }
+    let raw_body = res.body().await?;
+    log::debug!("Raw Body: {:?}", raw_body);
 
-    let body = res.unwrap().body().await.unwrap();
+    // let json_raw = r#"{ "origin": "123.123.123.133" }"#;
+    // let j: MyIp = serde_json::from_str(&json_raw).unwrap();
+    // log::debug!("Json Body: {:?}", j.origin);
 
-    log::debug!("Response: {:?}", body);
+    // let json_body = res.json::<MyIp>().await?;
+    let json_body: MyIp = serde_json::from_slice(&raw_body)?;
+    log::debug!("Json Body: {:?}", json_body.origin);
+
+    return Ok::<(), Box<dyn Error>>(());
 }
